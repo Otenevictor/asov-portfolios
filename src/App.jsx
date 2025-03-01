16a1b9eb1fc59b508a3afb9e6700832e1be76d91
Victor Otene <solomonotene36@gmail.com>
2025-03-01 10:46:22 +0100

    tailwind initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Navbar from "./component/nav"
 
 
 function App() {
 
   return (
     <>
+    <Navbar/>
+     <h1 class="text-3xl font-bold text-red-500 underline">
+    Hello world!
+  </h1>
     <div className="work">Hello World</div>
     </>
   )
